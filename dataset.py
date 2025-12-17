5a84d7bae4b815ee69d421a0bc82c58cb22a0ea9
Ziadelsamanody <ziadelsamanody@gmail.com>
2025-12-17 20:58:37 +0200

    refactor code and some function add utils easier usage

--- dataset.py
+++ dataset.py
@@ -5,10 +5,10 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision.ops import nms
 import pandas as pd 
 from torch_snippets import * 
+from utils import * 
 import numpy as  np 
 import cv2 as cv
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
 
 IMAGE_ROOT  = 'images/images'
 DF_RAW = pd.read_csv('df.csv')
@@ -42,34 +42,6 @@ ds = OpenImages(df=DF_RAW)
 # show(img , bbs=bbx, texts=clss, sz=10)
 
 
-def extract_candidates(img):
-    img_lbl, regions = selectivesearch.selective_search(img, scale=200, min_size=100)
-    img_area = np.prod(img.shape[:2])
-    candidates = []
-    for r in regions:
-        if r['rect'] in candidates : continue
-        if r['size'] < (0.05 * img_area) : continue
-        if r['size'] > (1 *img_area) : continue
-        x, y, w, h  = r['rect']
-        candidates.append(list(r['rect']))
-    return candidates
-
-def extract_iou(boxA, boxB, epsilon=1e-5):
-    x1 = max(boxA[0], boxB[0])
-    y1 = max(boxA[1], boxB[1])
-    x2 = min(boxA[2], boxB[2])
-    y2 = min(boxA[3], boxB[3])
-    width = (x2 - x1)
-    height = (y2 - y1)
-
-    if width < 0 or height < 0 :
-        return 0.0
-    area_overlap = width * height
-    area_a = (boxA[2] - boxA[0]) * (boxA[3] - boxA[1])
-    area_b = (boxB[2] - boxB[0]) * (boxB[3] - boxB[1])
-    area_combined = area_a + area_b - area_overlap
-    iou = area_overlap / (area_combined + epsilon)
-    return iou
 
 
 
@@ -123,4 +95,8 @@ targets = pd.DataFrame(flatten(CLSS), columns=['label'])
 label2target = {l:t for t, l in enumerate(targets['label'].unique())}
 target2label = {t:l for l,t in label2target.items()}
 background_class = label2target['background']
-print(background_class)
+# print(background_class)
+# creating training data
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229,0.224, 0.225])
+
